11be3d023b12b02a4a6cceab07d588e594e459a1
Pandiyaraj <ideas2it@muralis-MacBook-Pro.local>
2015-08-28 11:40:28 +0530

    AFNetworking added - Sudarsanam

---
+++ Parents/Parents/Services/LoginService.m
@@ -0,0 +1,13 @@
+//
+//  LoginService.m
+//  Parents
+//
+//  Created by ideas2it on 8/28/15.
+//  Copyright (c) 2015 Karthikeyan. All rights reserved.
+//
+
+#import "LoginService.h"
+
+@implementation LoginService
+
+@end
